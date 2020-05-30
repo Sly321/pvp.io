dbf50107fefa65f3c26b9bae4425bf894bddc683
Sven Liebig <liebigsv@gmail.com>
2020-05-30 21:04:18 +0200

    fix deprecated usage of query params

--- server/src/api/v1/rating.ts
+++ server/src/api/v1/rating.ts
@@ -5,11 +5,8 @@ const rating = express()
 
 const apiUrl = "https://eu.api.blizzard.com/"
 
-type FetchRatingOptions = {
+interface FetchRatingOptions extends RatingQueryParams {
     token: string
-    realmSlug: string
-    bracket: string
-    name: string
 }
 
 async function fetchRating({ token, realmSlug, bracket, name }: FetchRatingOptions)  {
@@ -18,26 +15,26 @@ async function fetchRating({ token, realmSlug, bracket, name }: FetchRatingOptio
     return fetch(url)
 }
 
-rating.get("/api/v1/rating", async function(req, res) {
+type RatingQueryParams = {
+    realmSlug: string
+    bracket: string
+    name: string
+}
+
+rating.get<{}, any, any, RatingQueryParams>("/api/v1/rating", async function(req, res) {
     if (!req.isAuthenticated()) {
         return res.sendStatus(401)
     }
-
-    // console.log("bracket: ", req.param("bracket"))
-    // console.log("realmSlug: ", req.param("realmSlug"))
-    // console.log("name: ", req.param("name"))
     
     const ratingRequest = await fetchRating({
         token: (req.user as any).token, 
-        realmSlug: req.param("realmSlug"), 
-        bracket: req.param("bracket"), 
-        name: req.param("name")
+        realmSlug: req.query.realmSlug, 
+        bracket: req.query.bracket, 
+        name: req.query.name
     })
 
-    // console.log(ratingRequest.status)
     if (ratingRequest.status === 200) {
         const rating = await ratingRequest.json()
-        // console.log(rating)
         return res.send(rating)
     }
 
